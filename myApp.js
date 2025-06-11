597ef8fc7a8818d0d1bf551d37b33c3394c131ad
anunyaOrange <anunyaOrange@proton.me>
2025-06-11 16:08:57 +0700

    init

--- myApp.js
+++ myApp.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 
-let Person;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, clientOptions);
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+}, { versionKey: false });
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
